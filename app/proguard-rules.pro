db26bd0b9279771a23f6b6cbc60052ab9b6dd638
Eric Cao <ericcaoworld@gmail.com>
2019-10-22 03:48:46 +0800

    Update to new version

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -3,7 +3,7 @@
 # in D:\3.Tool\Tools\Android\sdk/tools/proguard/proguard-android.txt
 # You can edit the include path and order by changing the proguardFiles
 # directive in build.gradle.
-#
+
 # For more details, see
 #   http://developer.android.com/guide/developing/tools/proguard.html
 
--- gradlew.bat
+++ gradlew.bat
@@ -21,15 +21,7 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto init
 
-echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-
-goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
